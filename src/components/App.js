7d45b496cf1a811836192391e0fb808ea81c1745
banudalamanli <banudalamanli@gmail.com>
2020-05-11 16:13:23 +0300

    Implement embedded video

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
-import VideoDetail from './VideoDetail'
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
-  onTermSubmit = async (term) => {
+  onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
         q: term,
@@ -17,15 +17,26 @@ class App extends React.Component {
   };
 
   onVideoSelect = video => {
-    this.setState({ selectedVideo: video })
-  }
+    this.setState({ selectedVideo: video });
+  };
 
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
