5656a0c5cb654dd3644f20a89a40fd1e2475ceae
banudalamanli <banudalamanli@gmail.com>
2020-05-11 13:03:25 +0300

    Set up axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyCGk2djYHJWapHhlMxyl8U4J4hdH_rH4RU'
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+})
+
